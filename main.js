7d9eaa9c46bab1b70c2d5133b8f5cee777172bc0
Augustin8273 <augubu9@gmail.com>
2022-11-07 14:50:52 +0200

    changing repository

--- main.js
+++
@@ -1,4 +0,0 @@
-const taskType=documnet.getElementById('taskType');
-const taskDescription=documnet.getElementById('taskDescription');
-const taskSubmit=documnet.getElementById('taskSubmit');
-const amount=documnet.getElementById('num');
