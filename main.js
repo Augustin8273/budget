a6d4b45b7f9056a9ef563dbd9b715fd2df6cfaf0
Augustin8273 <augubu9@gmail.com>
2022-11-07 14:23:48 +0200

    evolution

---
+++ main.js
@@ -0,0 +1,4 @@
+const taskType=documnet.getElementById('taskType');
+const taskDescription=documnet.getElementById('taskDescription');
+const taskSubmit=documnet.getElementById('taskSubmit');
+const amount=documnet.getElementById('num');
